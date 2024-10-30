dd7d90afb1e7a5594d79d4d6db931040053d8901
coldrice99 <coldrice99@gmail.com>
2024-10-30 17:40:48 +0900

    style : 줄바꿈

--- src/main/java/org/example/expert/domain/comment/service/CommentService.java
+++ src/main/java/org/example/expert/domain/comment/service/CommentService.java
@@ -29,8 +29,8 @@ public class CommentService {
     @Transactional
     public CommentSaveResponse saveComment(AuthUser authUser, long todoId, CommentSaveRequest commentSaveRequest) {
         User user = User.fromAuthUser(authUser);
-        Todo todo = todoRepository.findById(todoId).orElseThrow(() ->
-                new InvalidRequestException("Todo not found"));
+        Todo todo = todoRepository.findById(todoId)
+                .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         Comment newComment = new Comment(
                 commentSaveRequest.getContents(),
